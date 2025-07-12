123c19d2e72d0c2fd17e22644e51807c71b69cf3
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-12 13:09:28 +0530

    complete

--- Lec 26/index.js
+++ Lec 26/index.js
@@ -1,4 +1,22 @@
-console.log("Siya Ram")
+// console.log("Siya Ram")
 
-let num=101
-console.log(typeof (num))
+// let num=101
+// console.log(typeof (num))
+
+// let str = "RAM SIya Ram"
+// console.log(typeof(str))
+
+// let a = null
+// console.log(typeof(a))
+
+let stdInfo = {
+    name: 'Jai Siya Ram',
+    rollNo:'2023H1030030G'
+}
+console.log(stdInfo)
+
+let a = 4 == '4'
+let b = 4 === '4'
+
+console.log(a)
+console.log(b)
