1431637e96abcaa7d810c5351f4c89d175612d1b
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-13 12:07:45 +0530

    funtion:  Date | Math | String | Number

--- Lec 28/index.js
+++ Lec 28/index.js
@@ -1,7 +1,16 @@
-function totalMarks(sem1,  sem2,  sem3)
-{
-    let sum = sem1+sem2+sem3
-    alert("Total Sum is "+sum)
-}
+// function totalMarks(sem1,  sem2,  sem3)
+// {
+//     let sum = sem1+sem2+sem3
+//     alert("Total Sum is "+sum)
+// }
 
-totalMarks(100,20,3)
+// //totalMarks(100,20,3)
+// let num1, num2, num3
+
+// num1=Number(prompt())
+// num2=Number(prompt())
+// num3=Number(prompt())
+
+// totalMarks(num1,num2,num3)
+
+
