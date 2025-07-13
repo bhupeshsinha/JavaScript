8ea78cb4f5689cc73cc0f71b96f725b7818cd3cd
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-13 22:15:02 +0530

    Lec 29: Array And Objects in Javascript

---
+++ Lec 29/index.js
@@ -0,0 +1,10 @@
+console.log("Jai Siya Ram!")
+
+let arr=["Jai", "Siya ", "Ram ", "Hanuman", "Ram ji ke Doot", "Bajrang Bali"]
+console.log(arr[2])
+console.log(arr)
+
+let ar = [1,2,3,4,5,6,7,8]
+ar.push(9,10,11,12)
+ar.push([13,14,15])
+console.log(ar)
