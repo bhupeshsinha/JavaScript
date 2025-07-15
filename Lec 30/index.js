2728b773ce07fe4a3c196e14605bb8b0d58037f5
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-15 12:58:39 +0530

    Spread Operator Finished

--- Lec 30/index.js
+++ Lec 30/index.js
@@ -21,11 +21,11 @@ console.log("********")
 // sum(4,5,6)
 
 
-let obj = {
-    title: "Lecture 30",
-    tags:"lec30, rest, spread",
-    description:"Hello Dosto"
-}
+// let obj = {
+//     title: "Lecture 30",
+//     tags:"lec30, rest, spread",
+//     description:"Hello Dosto"
+// }
 
 // let {title, tags, description} = obj
 // console.log(title)
@@ -73,3 +73,18 @@ let obj = {
 // }
 
 // obj1(obj)
+
+// ***************************************************************************************************************************************************************************************************************************************
+
+// SPREAD OPERATOR
+//1. De-Grouping
+//2. Merge 2 array contents
+
+// let arr = ['Jai', 'Ram', 'Jai', 'Ram', 'Jai', 'Ram', 'Jai', 'Ram']
+// let arr2 = ['Hare', 'Ram','Hare', 'Ram','Hare', 'Ram','Hare', 'Ram',]
+
+// let newArray = [...arr, ...arr2]
+// let newArray2 = [...arr]
+// console.log(newArray)   //SPREAD OPTR
+// console.log(newArray2)  //SPREAD OPTR
+
