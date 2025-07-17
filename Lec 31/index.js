6a3a5fc1e7a62374dae25aa0737fba4211c5f80f
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-17 09:23:28 +0530

    Lec 32: Lexical Scoping

--- Lec 31/index.js
+++ Lec 31/index.js
@@ -5,3 +5,8 @@ console.log("**************")
 // 1. Global
 // 2. Block
 // 3. Function
+
+
+// 1. let:  within block
+// 2. var:  Global
+// 3. const:  within block
---
+++ Lec 32/index.js
@@ -0,0 +1,26 @@
+console.log("**************")
+console.log("Jai Siya Ram")
+console.log("**************")
+
+// {
+//     console.log(`GIVES UNDEFINED: Inner let ${a}`)
+//     var a ="Ram"
+//     console.log(`Inner let ${a}`)
+// }
+// console.log(`Outer let ${a}`)
+
+// {
+//     console.log(`GIVES ERROR: Inner let ${b}`)
+//     let b = "Jai Ram"
+//     console.log(`Inner let ${b}`)
+// }
+//console.log(`Error: Outer let ${b}`)
+
+
+function fun1()
+{
+    var name = "Ram Bhakt"
+    console.log(name)
+}
+
+fun1()
