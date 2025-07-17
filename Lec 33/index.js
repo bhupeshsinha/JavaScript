9483efa364a78777aed4c06397240ba34f3788b1
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-17 11:43:13 +0530

    How JS code Excute Internally | 2 Phases |  Parting(Memory Creation) | Execution

---
+++ Lec 33/index.js
@@ -0,0 +1,15 @@
+console.log("**************")
+console.log("Jai Siya Ram")
+console.log("**************")
+
+var num1=2;
+var num2=5;
+
+function add(a,b)
+{
+    var total = a+b;
+    return total;
+}
+
+var sum = add(num1, num2)
+console.log(sum)
