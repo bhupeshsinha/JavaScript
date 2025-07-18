0fa8ee3e62647f6838c8c7908e635a2409d7102d
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-18 11:36:16 +0530

    Hands On Firefox Debugger | 2 Phase Flow

--- Lec 33/index.js
+++ Lec 33/index.js
@@ -2,14 +2,36 @@ console.log("**************")
 console.log("Jai Siya Ram")
 console.log("**************")
 
-var num1=2;
-var num2=5;
+// var num1=2;
+// var num2=5;
 
-function add(a,b)
+// function add(a,b)
+// {
+//     var total = a+b;
+//     return total;
+// }
+
+// var sum = add(num1, num2)
+// console.log(sum)
+
+function one()
 {
-    var total = a+b;
-    return total;
+    var a= "hello1";
+    console.log("Inside One")
+
+    function two()
+    {
+        let b = "hello2";
+        console.log("Inside Two");
+
+        function three()
+        {
+            let c = "hello3";
+            console.log("Inside Three");
+        }
+        three();
+    }
+    two();
 }
 
-var sum = add(num1, num2)
-console.log(sum)
+one()
