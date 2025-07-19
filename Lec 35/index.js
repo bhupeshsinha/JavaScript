9ae88c4deb7e2c7a68e2cf6649389df01394a45d
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-19 11:03:53 +0530

    Implicit | Explicit | Example

--- Lec 35/index.js
+++ Lec 35/index.js
@@ -1,3 +1,22 @@
 console.log("**************")
 console.log("Jai Siya Ram")
 console.log("**************")
+
+// let a = 2;
+// let b = "4"  //Treated As String: Implicitily
+
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(a+b)
+// console.log(typeof(a+b))
+
+
+let a = 2;
+let b = Number("4");    //Treated As Number: Explicitily
+
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(a+b)
+console.log(typeof(a+b))
