62940b0e99568c19f494969de51d01d2acb0161c
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-19 11:19:07 +0530

    Abstract Operation| ToString | ToBoolean | ToNumber | ToPrimitive

--- Lec 35/index.js
+++ Lec 35/index.js
@@ -12,11 +12,34 @@ console.log("**************")
 // console.log(typeof(a+b))
 
 
-let a = 2;
-let b = Number("4");    //Treated As Number: Explicitily
+// let a = 2;
+// let b = Number("4");    //Treated As Number: Explicitily
 
 
-console.log(typeof(a))
-console.log(typeof(b))
-console.log(a+b)
-console.log(typeof(a+b))
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(a+b)
+// console.log(typeof(a+b))
+
+// console.log(3)
+// console.log(3+9)
+// console.log(3+9+"10")
+// console.log(3+9+"10"+100)
+// console.log("10"+100+222)
+
+
+// console.log(7+null)
+// console.log("7"+null)
+// console.log(7+true)
+// console.log(5-"56")
+// console.log("Jai Ram"-10)
+// console.log({}-5)
+// console.log([]-5)
+// console.log(5-undefined)
+
+
+// Abstract Operation
+// 1. ToPrimitive
+// 2. ToBoolean
+// 3. ToString
+// 4. ToNumber
