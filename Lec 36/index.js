03cc6e2083072bef49d1d1b084d91b0273944f60
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-21 19:26:32 +0530

    innerText Vs innerHTML vs TextContent

--- Lec 36/index.js
+++ Lec 36/index.js
@@ -3,9 +3,29 @@ console.log("Jai Siya Ram")
 console.log("**************")
 
 
-const heading = document.getElementById('heading')
-console.log(heading)
-console.log(heading.innerText)
+// const heading = document.getElementById('heading')
+// console.log(heading)
+// console.log(heading.innerText)
 
-heading.innerText = "Mere Shri Ram"
-console.log(heading.innerText)
+// heading.innerText = "Mere Shri Ram"
+// console.log(heading.innerText)
+
+
+// const heading = document.getElementsByTagName('h1')
+// // heading.innerText = "Mere Shri Ram"
+// console.log(heading[0].innerText)
+
+
+// let arr = [20,40,60,80,100];
+// console.log(arr)
+
+
+// const heading = document.getElementsByClassName('heelo')
+// // heading.innerText = "Mere Shri Ram"
+// console.log(heading)
+
+const heading = document.getElementsByClassName('heelo')
+// heading.innerText = "Mere Shri Ram"
+console.log(heading[0].innerHTML)
+console.log(heading[0].innerText)
+console.log(heading[0].textContent)
