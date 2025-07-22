8ee7f4ee665d7978ccae9c43bfd0a85b43497938
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-22 16:23:20 +0530

    Lec 37: Events in JS

---
+++ Lec 37/index.js
@@ -0,0 +1,9 @@
+console.log("**************")
+console.log("Jai Siya Ram")
+console.log("**************")
+
+
+const btn = document.getElementById('btn');
+btn.onclick = () => {
+    console.log('Jai Siya Ram from js file')
+}
