1f805d839067a23292974f0c61902044d34b10f2
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-22 16:27:51 +0530

    Event Listener

--- Lec 37/index.js
+++ Lec 37/index.js
@@ -3,7 +3,18 @@ console.log("Jai Siya Ram")
 console.log("**************")
 
 
+//Event Handler
+// const btn = document.getElementById('btn');
+// btn.onclick = () => {
+//     console.log('Jai Siya Ram from js file')
+// }
+
 const btn = document.getElementById('btn');
-btn.onclick = () => {
-    console.log('Jai Siya Ram from js file')
-}
+
+btn.addEventListener('click', function(){
+    console.log('Jai Siya Ram from js file, using event listener: NORMAL FUNCTION')
+})
+
+btn.addEventListener('click', () => {
+    console.log('Jai Siya Ram from js file, using event listener: ARROW FUNCTION')
+})
