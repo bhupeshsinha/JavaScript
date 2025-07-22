66182fdcdeb44745f251edc23eb98c18f0204ffe
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-07-22 17:22:58 +0530

    Event Listeners in JavaScript: Handling User Interactions Dynamically

--- Lec 37/index.js
+++ Lec 37/index.js
@@ -9,12 +9,50 @@ console.log("**************")
 //     console.log('Jai Siya Ram from js file')
 // }
 
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     console.log('Jai Siya Ram from js file, using event listener: NORMAL FUNCTION')
+// })
+
+// btn.addEventListener('click', () => {
+//     console.log('Jai Siya Ram from js file, using event listener: ARROW FUNCTION')
+// })
+
+
+// const btn = document.getElementById('btn');
+
+
+// btn.addEventListener('click', (eventObject) => {
+//     console.log(eventObject.target.type)
+//     console.log('Jai Siya Ram from js file, using event listener: ARROW FUNCTION')
+// })
+
+
+// const btn = document.getElementById('btn');
+
+
+// btn.addEventListener('click', (e) => {
+//     // e.target.innerText="change"
+//     console.log(e.target)
+//     console.log('Jai Siya Ram from js file, using event listener: ARROW FUNCTION')
+// })
+
+
+const body = document.querySelector("body")
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function(){
-    console.log('Jai Siya Ram from js file, using event listener: NORMAL FUNCTION')
-})
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
+    // e.target.innerText="change"
+    // alert("Jai Ram Jai Ram")
+
+    if(confirm("Color Change ?"))
+    {
+    body.style.backgroundColor = "orange"
+    console.log(e.target)
     console.log('Jai Siya Ram from js file, using event listener: ARROW FUNCTION')
+    }
+
+
 })
