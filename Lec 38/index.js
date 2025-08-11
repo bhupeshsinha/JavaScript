fae7c94c3bdaa3247429d902ffb7f171796f1595
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-08-11 18:23:13 +0530

    dragstart() | dragover() | dragenter() | dragleave()  | dragend()

--- Lec 38/index.js
+++ Lec 38/index.js
@@ -29,14 +29,27 @@ const items = document.getElementsByClassName("item")
 
 for(let item of items){
     item.addEventListener('dragstart', (e)=>{
-        console.log(e.target)
+        // console.log(e.target)
+        let data = e.target
         box2.addEventListener('dragover', (e)=>{
             e.preventDefault();
             e.stopPropagation();
         })
 
         box2.addEventListener("drop",()=>{
-            box2.append(e.target);
+            box2.append(data);
+            data='';
+        })
+
+
+        box1.addEventListener('dragover', (e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+        })
+
+        box1.addEventListener("drop",()=>{
+            box1.append(data);
+            data='';
         })
     })
 }
