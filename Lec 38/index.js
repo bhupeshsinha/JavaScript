9ff1209a9e62312094476b26cd2ee1f1f9a23b21
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-08-11 14:22:31 +0530

    Event Bubbling

--- Lec 38/index.js
+++ Lec 38/index.js
@@ -1,3 +1,18 @@
 console.log("**************")
 console.log("Jai Siya Ram")
 console.log("**************")
+
+const mainDiv = document.getElementById("mainDiv")
+const box = document.getElementById("div1")
+console.log(box)
+
+box.addEventListener('click', ()=>{
+    console.log("BOX")
+    // box.style.backgroundColor='red'
+})
+
+
+mainDiv.addEventListener('click', ()=>{
+    console.log("Main Div")
+    // box.style.backgroundColor='red'
+})
