2d88b5cabf3b26c66533c7cea9a5c36d85a2bad3
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-08-11 18:10:49 +0530

    Drag Down from one box to another box

--- Lec 38/index.js
+++ Lec 38/index.js
@@ -2,23 +2,41 @@ console.log("**************")
 console.log("Jai Siya Ram")
 console.log("**************")
 
-const mainDiv = document.getElementById("mainDiv")
-const box = document.getElementById("div1")
-console.log(box)
+// const mainDiv = document.getElementById("mainDiv")
+// const box = document.getElementById("div1")
+// console.log(box)
 
-box.addEventListener('click', ()=>{
-    console.log("BOX")
-    // box.style.backgroundColor='red'
-},true)
-
-
-
-mainDiv.addEventListener('click', ()=>{
-    console.log("Main Div")
-    // box.style.backgroundColor='red'
-},true)
+// box.addEventListener('click', ()=>{
+//     console.log("BOX")
+//     // box.style.backgroundColor='red'
+// },true)
 
 
-document.body.addEventListener('click', ()=>{
-    console.log("Body")
-},true)
+
+// mainDiv.addEventListener('click', ()=>{
+//     console.log("Main Div")
+//     // box.style.backgroundColor='red'
+// },true)
+
+
+// document.body.addEventListener('click', ()=>{
+//     console.log("Body")
+// },true)
+
+const box1 = document.querySelector(".box1")
+const box2 = document.querySelector(".box2")
+const items = document.getElementsByClassName("item")
+
+for(let item of items){
+    item.addEventListener('dragstart', (e)=>{
+        console.log(e.target)
+        box2.addEventListener('dragover', (e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+        })
+
+        box2.addEventListener("drop",()=>{
+            box2.append(e.target);
+        })
+    })
+}
