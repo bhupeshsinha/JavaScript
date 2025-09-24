ee93109b74bc707639ad2fa5388cbf2d40c78634
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-09-24 20:00:31 +0530

    3 different way to write Async-Await

--- Lec 43/index.js
+++ Lec 43/index.js
@@ -23,3 +23,32 @@
 // }
 
 // console.log(fun3())
+
+//1st way to write ASYNC
+async function fun1(){
+    return ("Ekdum Bdiya Company me place honga me!")
+};
+
+fun1().then((data)=>{
+    console.log(data);
+})
+
+
+//2nd way to write ASYNC
+function fun2(){
+    return Promise.resolve("Ekdum Bdiya Company me place honga me!")
+};
+fun2().then((data)=>{
+    console.log(data);
+});
+
+//3rd way to write ASYNC
+function fun3(){
+    return new Promise(function (resolve, reject){
+        resolve("Ekdum Bdiya Company me place honga me!");
+    })
+}
+
+fun3().then((data)=>{
+    console.log(data);
+})
