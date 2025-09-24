5b7fee01e97da95f6bfb24ab2162d19f4acc5e23
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-09-24 19:57:32 +0530

    Async + Await

---
+++ Lec 43/index.js
@@ -0,0 +1,25 @@
+// function fun1(){
+//     return ("Ekdum Bdiya Company me place honga me!")
+// };
+
+// console.log(fun1());
+
+
+// async function fun1(){
+//     return ("Ekdum Bdiya Company me place honga me!")
+// };
+
+// console.log(fun1());
+
+// function fun2(){
+//     return Promise.resolve("Ekdum Bdiya Company me place honga me!")
+// };
+// console.log(fun2());
+
+// function fun3(){
+//     return new Promise(function (resolve, reject){
+//         resolve("Ekdum Bdiya Company me place honga me!");
+//     })
+// }
+
+// console.log(fun3())
