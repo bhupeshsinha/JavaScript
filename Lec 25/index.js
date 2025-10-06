d133786628e72af02d25e970886aa4eaabcc5a8f
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-10-06 19:27:44 +0530

    complete

--- Lec 25/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Jai Siya Ram | Raam Doot Hanuman")
--- Lec 27/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Jai Jai Shri Ram")
-
-let name = prompt("Enter your name: ")
-alert("Your name is "+name)
-console.log("Your name is: ",name)
