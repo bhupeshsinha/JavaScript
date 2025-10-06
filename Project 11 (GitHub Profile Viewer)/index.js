47dc8b708404e80fb386e743310b3336fd71d86e
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-10-06 18:38:47 +0530

    complete

--- Project 11 (GitHub Profile Viewer)/index.js
+++ Project 11 (GitHub Profile Viewer)/index.js
@@ -10,8 +10,8 @@ let user = document.getElementById('userID');
 
 async function fetchUser(userId) {
     let response = await fetch(`https://api.github.com/users/${userId}`);
-    let result = await response.json();
-    console.log(result);
+    let result = await response.json();   
+    displayUser(result);
 }
 
 
@@ -19,5 +19,66 @@ document.getElementById('btn').addEventListener('click', ()=>{
     let userId = user.value
     fetchUser(userId);
 })
+
+function displayUser(result){
+    const {
+        avatar_url, 
+        name, 
+        bio, 
+        followers, 
+        following, 
+        public_repos,
+        html_url
+    } = result;
+    
+    if(!avatar_url){
+                document.getElementById('userProfile').innerHTML = `<h1>USER NOT FOUND</h1>`
+                return;
+    }
+
+    if(!bio){
+        bio = '';
+    }
+    document.getElementById('userProfile').innerHTML = 
+        `           
+                    <div class="userInfo">
+                            <img src=${avatar_url} class="userImg" alt="">
+                            <div class="userDetail">
+                                <p class="userName">${name}</p>
+                                <p class="userBio">${bio}</p>
+                            </div>
+                    </div>
+
+                    <div class="userFollow">
+                            <div class="Follower">
+                                <div class="repo">
+                                    <p>Follower</p>
+                                    <p>${followers}</p>
+                                </div>
+
+                                <div class="repo">
+                                    <p>Following</p>
+                                    <p>${following}</p>
+                                </div>
+
+                                <div class="repo">
+                                    <p>Repo</p>
+                                    <p>${public_repos}</p>
+                                </div>
+                            </div>
+
+                            <a href=${html_url} target='_blank' class="VisitProfile">
+                                <div >
+                                    Visit Profile
+                                </div>                                
+                            </a>
+
+
+                    </div>
+        `;
+    
+
+
+}
 
 // GITHUB API:  https://api.github.com/users/userId
