74319f69bfbe5f8859751b10a0914302f13f29d4
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-10-08 15:38:27 +0530

    Called API

---
+++ Project 12 (Image Finder)/index.js
@@ -0,0 +1,41 @@
+
+const ACCESS_TOKEN = 'jxTWcfzcIQb-V2t3Z6mANMt7I7hl7XcefBhKGRTk4W4';
+
+let keyword = document.getElementById('keyword');
+
+document.getElementById('btn').addEventListener('click', ()=>{
+    let val = keyword.value;
+    fetchImg(val);
+}) 
+
+
+async function fetchImg(val) {
+    //API:  https://api.unsplash.com/search/photos?query=shoes&client_id=jxTWcfzcIQb-V2t3Z6mANMt7I7hl7XcefBhKGRTk4W4
+    let response = await fetch(`https://api.unsplash.com/search/photos?query=${val}&client_id=${ACCESS_TOKEN}`);
+    let result = await response.json();
+    displayImg(result);
+}
+
+
+function displayImg(res){
+    // console.log(keyword.value);
+    res.results.map(data => {
+        let div = document.createElement('div');
+        div.setAttribute('class', "singleImg");
+
+        div.innerHTML = `
+        <div class="singleImg">
+                <div class="firstDiv">
+                    <img src=${data.user.profile_image.large} alt="" class="userImg">
+                    <p class="userName">${data.user.name}</p>
+                </div>
+                <div class="secondDiv">
+                    <img src=${data.urls.regular} alt="" class="mainImg">
+                    <p class="imgDes">${data.alt_description}</p>
+                </div>
+        </div>
+        `;
+        // console.log(div);
+        document.getElementById('allImg').appendChild(div);
+    })    
+}
