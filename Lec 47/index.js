4018b67af15bb79202e09ef391235754f8424e25
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-10-12 16:57:28 +0530

    practicing

--- Lec 47/index.js
+++ Lec 47/index.js
@@ -1,1 +1,14 @@
-sessionStorage.
+// document.getElementById("btn").addEventListener('click', ()=>{
+//     sessionStorage.setItem("store", "Jai Siya Ram")
+// })
+
+
+// document.getElementById("btn").addEventListener('click', ()=>{
+//     localStorage.setItem("store", "Jai Siya Ram")
+// })
+
+
+document.getElementById("btn").addEventListener('click', ()=>{
+    let data = localStorage.getItem("store")
+    console.log(data)
+})
