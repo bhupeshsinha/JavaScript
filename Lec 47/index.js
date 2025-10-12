708510236150645a4729d7768c59866ec633c60a
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-10-12 21:40:54 +0530

    local storage

--- Lec 47/index.js
+++ Lec 47/index.js
@@ -8,7 +8,19 @@
 // })
 
 
-document.getElementById("btn").addEventListener('click', ()=>{
-    let data = localStorage.getItem("store")
-    console.log(data)
+// document.getElementById("btn").addEventListener('click', ()=>{
+//     let data = localStorage.getItem("store")
+//     console.log(data)
+// })
+
+// document.getElementById("btn").addEventListener('click', ()=>{
+//     localStorage.clear()
+   
+// })
+
+document.getElementById("in").addEventListener('input', (e)=>{
+//    console.log(e.target.value);
+    localStorage.setItem('data', e.target.value);
+    document.getElementById('h1').innerText = localStorage.getItem('data');
 })
+document.getElementById('h1').innerText = localStorage.getItem('data');
