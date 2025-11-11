28c6b9afdee39aaf20f3b65deefe7d57eda110a9
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-11-11 17:09:48 +0530

    "remove task" button fixed

--- Project 14 (To Do List App)/index.js
+++ Project 14 (To Do List App)/index.js
@@ -1,8 +1,14 @@
 let todoInput = document.getElementById('todoInput');
 
-function removeTodo(){
+function removeTodo(id){
     console.log("To do removed");
+    console.log(id);
+    let toDelete = document.getElementById(id);
+    toDelete.remove();
+    alert("Task Removed Successfully");
 }
+
+let id = 1;
 
 document.getElementById('addBtn').addEventListener('click', (e)=>{
     e.preventDefault();
@@ -10,12 +16,15 @@ document.getElementById('addBtn').addEventListener('click', (e)=>{
     console.log(todoVal);
     let div = document.createElement('div');
     div.setAttribute('class', 'singleTodo');
+    // div.setAttribute("id", id++);
+    div.setAttribute("id", id);
     div.innerHTML = `
                 <h2 id="taskToDo">${todoVal}</h2>
                 <input type="checkbox" name="" id="">
-                <button onclick = removeTodo() >Remove Task</button>
+                <button onclick = removeTodo(${id}) >Remove Task</button>
             `;
     console.log(div);
     document.getElementById('allTodo').appendChild(div);
+    id++;
     todoInput.value = '';
 });
