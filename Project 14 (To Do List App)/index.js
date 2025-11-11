743d120384415a49f6249f4ffc28620526fade86
bhupeshsinha <bhupeshsinha247@gmail.com>
2025-11-11 18:14:10 +0530

    alternate method

--- Project 14 (To Do List App)/index.js
+++ Project 14 (To Do List App)/index.js
@@ -1,14 +1,48 @@
 let todoInput = document.getElementById('todoInput');
 
-function removeTodo(id){
-    console.log("To do removed");
-    console.log(id);
-    let toDelete = document.getElementById(id);
-    toDelete.remove();
-    alert("Task Removed Successfully");
+{
+// let id = 1;
+
+// function removeTodo(id){
+//     console.log("To do removed");
+//     console.log(id);
+//     let toDelete = document.getElementById(id);
+//     toDelete.remove();
+// }
+
+// function markTodo(id){
+//     // console.log("To do marked");
+//     // let toMark = document.getElementById(id);
+//     // toMark.children[0].style.textDecoration = "line-through";
+//     // alert("Task Marked Successfully");
+
+//     let div = document.getElementById(id);
+//     div.children[0].classList.toggle('completed');
+// }
+
+
+// document.getElementById('addBtn').addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     let todoVal = todoInput.value;
+//     console.log(todoVal);
+//     let div = document.createElement('div');
+//     div.setAttribute('class', 'singleTodo');
+//     // div.setAttribute("id", id++);
+//     div.setAttribute("id", id);
+//     div.innerHTML = `
+//                 <h2 id="taskToDo">${todoVal}</h2>
+//                 <input type="checkbox" name="" id="" onclick = markTodo(${id}) >
+//                 <button onclick = removeTodo(${id}) >Remove Task</button>
+//             `;
+//     console.log(div);
+//     document.getElementById('allTodo').appendChild(div);
+//     id++;
+//     todoInput.value = '';
+// });
 }
 
-let id = 1;
+
+
 
 document.getElementById('addBtn').addEventListener('click', (e)=>{
     e.preventDefault();
@@ -17,14 +51,24 @@ document.getElementById('addBtn').addEventListener('click', (e)=>{
     let div = document.createElement('div');
     div.setAttribute('class', 'singleTodo');
     // div.setAttribute("id", id++);
-    div.setAttribute("id", id);
+    // div.setAttribute("id", id);
     div.innerHTML = `
                 <h2 id="taskToDo">${todoVal}</h2>
                 <input type="checkbox" name="" id="">
-                <button onclick = removeTodo(${id}) >Remove Task</button>
+                <button >Remove Task</button>
             `;
-    console.log(div);
+
+    let btn = div.getElementsByTagName('button');
+    let inp = div.getElementsByTagName('input');
+
+    btn[0].addEventListener('click', (e)=>{{
+        e.target.parentNode.remove();
+    }});
+
+    inp[0].addEventListener('click', (e)=>{
+        e.target.previousElementSibling.classList.toggle('completed');
+    });
+
     document.getElementById('allTodo').appendChild(div);
-    id++;
     todoInput.value = '';
 });
